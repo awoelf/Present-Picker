e099d67835024c03068ec99d4e1764ed64420c3f
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-12-07 10:42:20 -0600

    Fixed some syntax errors. Added Profile.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,37 @@
-import React from `react`;
+import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from `@apollo/client`;
-import {setContext} from `@apollo/client/link/context`;
-import {BrowserRouter as Router, Routes, Route} from `react-router-dom`;
-import './App.css';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import Login from `./pages/Login`;
-import Profile from `./pages/Profile`;
-import SignUp from `./pages/SignUp`;
-import Search from `./pages/Search`;
-import Dashboard from `./pages/Search`;
-import Header from `./components/Header`;
-import Footer from `./components/Footer`;
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Search from "./pages/Search";
+import Dashboard from "./pages/Search";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 // From 21 MERN/26/Solved/client/src/App.js
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -44,38 +44,23 @@ const client = new ApolloClient({
 
 function App() {
   return (
-   <ApolloProvider client={client}>
-    <Router>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Routes>
-            <Route 
-            path="/" 
-            element={<Dashboard />}
-            />
-            <Route 
-            path="/login" 
-            element={<Login />}
-            />
-            <Route 
-            path="/signup" 
-            element={<SignUp />}
-            />
-            <Route 
-            path="/me" 
-            element={<Profile />}
-            />
-            <Route 
-            path="/search" 
-            element={<Search />}
-            />
-          </Routes>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/me" element={<Profile />} />
+              <Route path="/search" element={<Search />} />
+            </Routes>
           </div>
           <Footer />
         </div>
-    </Router>
-   </ApolloProvider>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,1 +1,64 @@
-// Here is a profile page
+import React from 'react';
+import {Navigate, useParams} from 'react-router-dom';
+import {useQuery} from '@apollo/client';
+
+import {QUERY_USER, QUERY_ME} from '../utils/queries';
+import Auth from '../utils/auth';
+
+const Profile = () => {
+    const {email: useParams} = useParams();
+
+    const {loading, data} = useQuery(useParams ? QUERY_USER : QUERY_ME, {
+        variables: {email: useParams},
+    });
+
+    const user = data?.me || data?.user || {};
+    if (Auth.loggedIn() && Auth.getProfile().data.email === userParams) {
+        return <Navigate to="/me" />;
+    }
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!user.email) {
+        return (
+            <h4>
+                You need to be logged in to see this. Use the navigation links above to
+                sign up or log in!
+            </h4>
+        );
+    }
+
+    return (
+    <div>
+      <div className="flex-row justify-center mb-3">
+       <div className="card mb-3" style="max-width: 540px;">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="..." className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <div className="mb-3 row">
+                            <label for="staticEmail" className="col-sm-2 col-form-label">Email</label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control-plaintext" id="staticEmail" value="email@example.com"/>
+                            </div>
+                        </div>
+                        <div className="mb-3 row">
+                            <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                            <div className="col-sm-10">
+                                <input type="password" className="form-control" id="inputPassword"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    );
+};
+
+export default Profile;
