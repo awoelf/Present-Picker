c2aa9fdd8ea482dceb91dabed5f55dfb22eef966
Katie Maddox <kmaddox2122@gmail.com>
2022-12-07 14:27:46 -0600

    updated mutations, queries, resolvers, and type defs

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,10 +1,39 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const QUERY_USER = gql`
   query user($email: String!) {
     user(email: $email) {
       _id
       email
+      lists {
+        _id
+        listText
+      }
+    }
+  }
+`;
+
+export const QUERY_LISTS = gql`
+  query getLists {
+    lists {
+      _id
+      listText
+      email
+    }
+  }
+`;
+
+export const QUERY_SINGLE_LIST = gql`
+  query getSingleList($listId: ID!) {
+    list(listId: $listId) {
+      _id
+      listText
+      email
+      items {
+        _id
+        itemText
+        email
+      }
     }
   }
 `;
@@ -14,6 +43,11 @@ export const QUERY_ME = gql`
     me {
       _id
       email
+      lists {
+        _id
+        listText
+        email
+      }
     }
   }
 `;
