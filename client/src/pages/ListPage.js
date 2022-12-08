d7078e1ca953f767baed854a54a5d8a79676218a
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-12-08 13:31:12 -0600

    Added to dashboard, email, listpage. Edited Profile and Nav.

---
+++ client/src/pages/ListPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ListPage() {
+  return (
+    <div className="container">
+      <h1 className="text-center">List Page</h1>
+    </div>
+  );
+}
+
+export default ListPage;
