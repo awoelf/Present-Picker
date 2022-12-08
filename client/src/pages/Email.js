676cc8093edbc2d789361596cc7d0edc00081754
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-12-08 13:57:46 -0600

    Added to pages. Fixed Dashboard route.

---
+++ client/src/pages/Email.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Email() {
+  return (
+    <div className="container">
+      <h1 className="text-center">Email</h1>
+    </div>
+  );
+}
+
+export default Email;
