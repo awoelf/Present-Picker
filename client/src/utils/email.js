d138a98ca21e0ef3cc80470c05879835e2b69816
awoelf <98348782+awoelf@users.noreply.github.com>
2022-12-08 22:57:40 -0600

    Set up email and shopping utils. created Result and SearchItem components.

--- client/src/utils/email.js
+++ client/src/utils/email.js
@@ -3,9 +3,10 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendEmail = (recipients, subject, html) => {
+    // Email content
     const msg = {
         to: recipients,
-        from: 'alexis@presentpickerapp.com', // Change to your verified sender
+        from: 'alexis@presentpickerapp.com',
         subject: subject,
         html: html,
       }
