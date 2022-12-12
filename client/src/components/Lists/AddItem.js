c4f41427d6a52f958d806db5884b681968fb3981
awoelf <98348782+awoelf@users.noreply.github.com>
2022-12-12 13:56:39 -0600

    AddItem needs formatting

--- client/src/components/Lists/AddItem.js
+++ client/src/components/Lists/AddItem.js
@@ -3,39 +3,23 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 // Component for choosing to input item details manually or search for item
-const AddItem = (props) => {
-  const [show, setShow] = useState(false);
+const AddItem = ({show, setShow}) => {
   const handleClose = () => setShow(false);
-
-  const handleSearchItem = () => {
-    handleClose;
-    props.showSearchItem(true);
-  };
-
-  const handleInputItem = () => {
-    handleClose;
-    props.showInputItem(true);
-  };
   return (
-    <div
-      className='modal show'
-      style={{ display: 'block', position: 'initial' }}
-    >
-      <Modal.Dialog show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Add Item</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <Button onClick={handleSearchItem}>
+          <Button >
             <i class='bi bi-gift'></i>Search for an item
           </Button>
-          <Button onClick={handleInputItem}>
+          <Button >
             <i class='bi bi-ui-checks-grid'></i>Input item details
           </Button>
         </Modal.Body>
-      </Modal.Dialog>
-    </div>
+      </Modal>
   );
 };
 
--- client/src/pages/ListPage.js
+++ client/src/pages/ListPage.js
@@ -9,6 +9,9 @@ import {
   QUERY_SINGLE_LIST,
   QUERY_LISTS,
 } from "../utils/queries";
+import AddItem from "../components/Lists/AddItem";
+import InputItem from "../components/Lists/InputItem";
+import SearchItem from "../components/Lists/SearchItem";
 
 // import { Navigate, useParams } from "react-router-dom";
 // import { useQuery } from "@apollo/client";
@@ -60,8 +63,13 @@ function ListPage() {
   //     </h4>
   //   );
   // }
+  const [showAddItem, setShowAddItem] = useState(false);
+  const [showInputItem, setShowInputItem] = useState(false);
+  const [showSearchItem, setShowSearchItem] = useState(false);
 
-  const handleAddItem = () => {};
+  const handleAddItem = () => {
+    setShowAddItem(true);
+  };
 
   return (
     <div className="card">
@@ -79,6 +87,14 @@ function ListPage() {
           </Dropdown.Menu>
         </Dropdown>
         {/* Add item button will open the InputItem component */}
+        <AddItem 
+          show={showAddItem}
+          setShow={setShowAddItem}
+        />
+        {/* <InputItem 
+
+        />
+        <SearchItem /> */}
         <div className={currentTheme}>
           <a href="#" className="btn add-item" onClick={handleAddItem}>
             <svg
@@ -93,21 +109,6 @@ function ListPage() {
               <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
             </svg>{" "}
             Add Item
-          </a>
-          {/* Search item button will open the SearchItem component */}
-          <a href="#" className="btn add-item">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-plus-square"
-              viewBox="0 0 16 16"
-            >
-              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-            </svg>{" "}
-            Search Item
           </a>
           <a href="#" className="btn save-list">
             <svg
